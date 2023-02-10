b128b3ea34a8c46d8d2df9c5ddaddaec9cbfc645
vatsalthummar <125036893+vatsalthummar@users.noreply.github.com>
2023-02-10 19:19:04 +0530

    Add files via upload

---
+++ p50.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+printf("DATE:15/12/2022\nNAME:Thummar Vatsal\nfind the product of two floating point numbers\n");
+float a,b;
+printf("Enter two numbers\n");
+scanf("%f %f",&a,&b);
+printf("their product is\n%f",a*b);
+    return 0;
+}
